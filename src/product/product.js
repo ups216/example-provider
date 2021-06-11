3c8c450767fc185cbeac65e8fd79c63ef6e37dd5
Lei Xu <leixu@leixu-macpro16>
2021-06-11 17:23:50 +0800

    fix : add 'name' back

--- src/product/product.js
+++ src/product/product.js
@@ -2,7 +2,7 @@ class Product {
     constructor(id, type, name, version) {
         this.id = id;
         this.type = type;
-        //this.name = name;
+        this.name = name;
         this.version = version;
     }
 }
